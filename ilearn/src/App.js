1bd1d3c4306079f7e7c2b6fa2f8f711580df79d2
Riya Jain <riyajn2001@gmail.com>
2021-10-15 15:10:39 +0530

    Added the center box element

--- ilearn/src/App.js
+++ ilearn/src/App.js
@@ -1,11 +1,13 @@
 import Sidebar from "./Sidebar";
-import ProgressBar from "./ProgressBar"
+import ProgressBar from "./ProgressBar";
+import CenterBar from "./CenterBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <CenterBar />
       <ProgressBar />
     </div>
   );
---
+++ ilearn/src/CenterBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './CenterBar.css';
+
+function centerBar() {
+    return (
+        <div className="centerBar">
+            <img src="/assets/images/Teaching-rafiki.png" alt="teaching" className="centerImage"></img>
+            <div className="vl"></div>
+            <div className="paragraph">
+                <p>Hey there!</p>
+                <p>Let's begin the learning.</p>
+            </div>
+        </div>
+    )
+}
+
+export default centerBar;
