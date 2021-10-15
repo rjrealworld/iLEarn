878ff55b6d7eed31c70e48e91b036154b7f5b197
Riya Jain <riyajn2001@gmail.com>
2021-10-15 13:05:41 +0530

    Edited the left sidebar with css and fontawesome icons

--- ilearn/src/Sidebar.js
+++ ilearn/src/Sidebar.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import BookIcon from '@mui/icons-material/Book';
-import EventIcon from '@mui/icons-material/Event';
-import ContactSupportIcon from '@mui/icons-material/ContactSupport';
-import FeedIcon from '@mui/icons-material/Feed';
-import LogoutIcon from '@mui/icons-material/Logout';
-import './Sidebar.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookOpen, faQuestionCircle, faCalendarAlt, faCommentAlt, faRssSquare, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
+import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <img src="./assets/icon-iLEarn.png" alt="icon-iLEarn"/>
-            <BookIcon />
-            <EventIcon />
-            <ContactSupportIcon />
-            <FeedIcon />
-            <LogoutIcon />
+            <img src="/assets/icon-iLEarn.png" alt="icon-iLEarn" className="sidebar-logo"/>
+            <FontAwesomeIcon icon={faQuestionCircle} className="sidebar-icons" />
+            <FontAwesomeIcon icon={faBookOpen} className="sidebar-icons" />
+            <FontAwesomeIcon icon={faCalendarAlt} className="sidebar-icons" />
+            <FontAwesomeIcon icon={faRssSquare} className="sidebar-icons" />
+            <FontAwesomeIcon icon={faCommentAlt} className="sidebar-icons" />
+            <FontAwesomeIcon icon={faTimesCircle} className="sidebar-icons" />
         </div>
     )
 }
