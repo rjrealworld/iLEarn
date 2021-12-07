19b3db090a942643254c2351642804e4376a1db6
Raag98 <raianurag9800@gmail.com>
2021-12-07 20:22:04 +0530

    Add Button inserted

--- react-sidebar/src/DashBoard/Pages/Calender.js
+++ react-sidebar/src/DashBoard/Pages/Calender.js
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MotionHoc from "./MotionHoc";
+import Add from "../../assets/Add.svg";
 import "./Calender.css";
+import db from "../.././firebase";
 import EventCard from "../Cards/EventCard";
+import { collection, getDocs, doc, setDoc } from "firebase/firestore";
 
 const CalenderComponent = () => {
-  const description = "Introduction to web development. Learn the basics of HTML, CSS, and JavaScript. This course covers the basic building blocks of a website: the HTML document, the head, the body, and the content.";
   const imgURL = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/music-event-poster-design-template-56512e03cadd02795cfa1aeb1d1da465_screen.jpg?ts=1567089463";
+
+  const ref = collection(db, "events");
+  const [eventList, setEventList] = useState([]);
+
+  const handleClick = ({}) => {
+    setDoc(doc(db, "events", "Event-t"), {
+      'Name': "Temp",
+      'OrganisedBy': "Hub",
+      'Poster': imgURL,
+      'Start': "2020-06-01",
+      'End': "2020-06-03",
+      'Venue': "OAT",
+      'count': 0,
+    });
+  };
+
+  useEffect(() => {
+    getDocs(ref).then((snapshot) => {
+      snapshot.forEach((event) => {
+        console.log(event.id, " ", event.data());
+        setEventList((d) => {
+          return [...d, {'id': event.id, 'data': event.data()}];
+        });
+      });
+    });
+    console.log(eventList);
+  }, []);
 
   return (
     <div>
       <h1>Events</h1>
       <div className="container-events">
         <EventCard
-          name="Web Dev Basics"
-          date="05/01/2020"
-          description={description}
+          name="Event"
+          date="2020-06-01"
+          description="Description"
           img={imgURL}
-          alt="img"
+          alt="altText"
         />
+        {eventList.map((evnt) => {
+          <EventCard
+            name={evnt.data.Name}
+            date="2020-06-01"
+            description={evnt.data.OrganisedBy}
+            img={evnt.data.Poster}
+            alt={evnt.id}
+          />
+        })}
+      </div>
+      <div className="float" onClick={handleClick}>
+        <img src={Add} className="my-float" />
       </div>
     </div>
   );
--- react-sidebar/src/DashBoard/Pages/Documents.js
+++ react-sidebar/src/DashBoard/Pages/Documents.js
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from "react";
 import MotionHoc from "./MotionHoc";
 import "./Document.css";
+import Add from "../../assets/Add.svg";
 import db from "../.././firebase";
-import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, getDocs, doc, setDoc } from "firebase/firestore";
 import BookCard from "../Cards/BookCard";
 
 const DocumentsComponent = () => {
   const ref = collection(db, "books");
   const [docs, setDocs] = useState([]);
 
+  const handleClick = ({}) => {
+    // console.log("clicked");
+    setDoc(doc(db, "books", "Book-t"), {
+      'Author': "Author-temp",
+      'Title': "Book-temp",
+      'Cover': "",
+      'E-Book': "",
+      'SubjectID': "",
+      'Tags': ["#pdf", "book"],
+      'Title': "Book-temp-title",
+      'User-Price': [{'Price': 800, 'User': "Anonymous"}, {'Price': 650, 'User': "Anon"}],
+    });
+  }
+
   useEffect(() => {
-    // const t = [];    
     getDocs(ref)
       .then((snapshot) => {
         snapshot.forEach((doc) => {
-          // console.log(doc.data());
-          // t.push(doc.data());
+          console.log(doc.id, ' ' , doc.data());
           setDocs(d => {
-            return [...d, doc.data()];
+            return [...d, {'id': doc.id, 'data': doc.data()}];
           })
         });
-        // console.log(docs);
     });
-    // setDocs(t);
     console.log(docs);    
   }, []);
 
@@ -33,16 +44,19 @@ const DocumentsComponent = () => {
         <div className="row-book">
           {docs.map((doc) => (
             <BookCard
-              title={doc.Title}
-              author={doc.Author}
+              title={doc.data.Title}
+              author={doc.data.Author}
               rupess="&#x20B9;"
               originalPrice="550"
               newPrice="200"
-              alt={doc.Title}
-              images={doc.Cover}
+              alt={doc.id}
+              images={doc.data.Cover}
             />
           ))}
         </div>
+      </div>
+      <div className="float" onClick={handleClick}>
+            <img src={Add} className="my-float" />
       </div>
     </div>
   );
