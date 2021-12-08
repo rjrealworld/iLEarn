ab69877068c5778165cc4fd82ffda64c89b82102
Raag98 <raianurag9800@gmail.com>
2021-12-09 02:49:42 +0530

    Updated Profile Card

--- react-sidebar/src/DashBoard/Pages/Dashboard/userCards.js
+++ react-sidebar/src/DashBoard/Pages/Dashboard/userCards.js
@@ -1,47 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
+import {useAuth} from "../../../contexts/AuthContext";
 import './userCards.css';
 
-class UserCards extends Component {
-  state = {
-    data: [],
-    per: 1,
-    page: 1,
-    total_pages: null
-  };
+const UserCards = () => { 
 
-  uppercase = (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  };
-
-  loadData = () => {
-    const { per, page, data } = this.state;
-    const endpoint = `https://randomuser.me/api/?nat=us&results=${per}&page=${page}`;
-    fetch(endpoint)
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({
-          data: [...data, ...json.results],
-          scrolling: false,
-          total_pages: json.info.results
-        });
-      });
-  };
-
-  loadMore = () => {
-    this.setState(
-      (prevState) => ({
-        page: prevState.page + 1,
-        scrolling: true
-      }),
-      this.loadData
-    );
-  };
-
-  componentDidMount() {
-    this.loadData();
-  }
-
-  render() {
+  const { currentUser } = useAuth();
+  
     return (
       <div className="user-card">
             <div className="card-body">
@@ -49,19 +13,18 @@ class UserCards extends Component {
               <h3>Profile</h3></center>
               <div className="avatar">
                 <img
-                  src="https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk="
+                  src={currentUser.photoURL}
                   className="card-img-top"
                   alt=""
                 />
               </div>
               <center>
-              <h5 className="card-title">Riya Jain</h5>
-              <p className="card-text">rj.riyajain09@gmail.com</p>
+              <h5 className="card-title">{currentUser.displayName}</h5>
+              <p className="card-text">{currentUser.email}</p>
               </center>
             </div>
           </div>
-    );
-  }
+  );
 }
 
 export default UserCards;
