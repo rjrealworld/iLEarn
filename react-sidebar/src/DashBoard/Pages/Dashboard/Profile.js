2eda1581bd3898e5e0d242b2bd4d29c2443485cf
Riya Jain <riyajn2001@gmail.com>
2021-12-09 11:00:47 +0530

    Edited the edit profile form

--- react-sidebar/src/DashBoard/Pages/Dashboard/Profile.js
+++ react-sidebar/src/DashBoard/Pages/Dashboard/Profile.js
@@ -1,28 +1,29 @@
 import React from "react";
 import "./Profile.css";
 
-
 const Profile = () => {
   return (
     <div class="profile">
       <div class="content">
         <h1>Edit Profile</h1>
         <form action="">
+          <div className="field">
             <div class="grid-35">
               <label for="avatar">Your Photo</label>
             </div>
             <div class="grid-65">
               <span class="photo" title="Upload your Avatar!"></span>
-              <input type="file" class="btn" />
             </div>
-                   
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="name">Name</label>
             </div>
             <div class="grid-65">
               <input type="text" id="name" tabindex="2" />
             </div>
-          
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="description">About you</label>
             </div>
@@ -35,14 +36,16 @@ const Profile = () => {
                 tabindex="3"
               ></textarea>
             </div>
-          
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="email">Email Address</label>
             </div>
             <div class="grid-65">
               <input type="email" id="email" tabindex="6" />
             </div>
-          
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="phone">Phone Number</label>
             </div>
@@ -55,7 +58,8 @@ const Profile = () => {
                 pattern="[1-9]{1}[0-9]{9}"
               />
             </div>
-          
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="qualification">Year</label>
             </div>
@@ -71,24 +75,25 @@ const Profile = () => {
                 <option value="fifth-year">5th year</option>
               </select>
             </div>
-          
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="linkedin">LinkedIn URL</label>
             </div>
             <div class="grid-65">
               <input type="text" id="linkedin" tabindex="12" />
             </div>
-          
+          </div>
+          <div className="field">
             <div class="grid-35">
               <label for="github">Github URL</label>
             </div>
             <div class="grid-65">
               <input type="text" id="github" tabindex="13" />
             </div>
-          
-            <input type="button" class="Btn cancel" value="Cancel" />
-            <input type="submit" class="Btn" value="Save Changes" />
-          
+          </div>
+          <input type="button" class="Btn cancel" value="Cancel" />
+          <input type="submit" class="Btn" value="Save Changes" />
         </form>
       </div>
     </div>
